db81b26df20874ca45c6ba87cb04d3e22c3fb647
kb0304 <karan@Karans-MacBook-Air.local>
2015-05-15 21:50:49 +0530

    formatting of Question model's display at admin page

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from polls.models import Question
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields':['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
 # Register your models here.
