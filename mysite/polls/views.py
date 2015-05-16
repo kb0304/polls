6b2294e90399e18aad20c8cbda47132c6b3b5b74
kb0304 <karan@Karans-MacBook-Air.local>
2015-05-16 06:04:31 +0530

    added view display question

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from polls.models import Question
-from django.template import RequestContext, loader
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s."% question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request, 'polls/detail.html',{'question':question})
+
 
 def results(request, question_id):
     response = "You're looking at the result of question %s."
@@ -15,7 +16,5 @@ def vote(request, question_id):
 
 def index(request):
     latest_question_list= Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request,{'latest_question_list':latest_question_list,
-    })
-    return HttpResponse(template.render(context))
+    context = {'latest_question_list':latest_question_list}
+    return render(request, 'polls/index.html' ,context)
